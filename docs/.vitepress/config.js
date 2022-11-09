2e265b06273a42819c701bed6e3c08bb8efbf752
周陆陆 <zhoululu@aegis-data.cn>
2022-11-09 18:26:20 +0800

    增加导航

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,28 @@
-export default {
+import { defineConfig } from 'vitepress'
+export default defineConfig({
   title: '笔记',
   description: '笔记',
   base: '/note/',
   themeConfig: {
-    outline: 'deep'
+    outline: 'deep',
+    nav: [
+      { text: 'web前端', link: '/web/' }
+    ],
+    sidebar: {
+      '/web/': [
+        {
+          text: 'web前端',
+          items: [
+            { text: 'unocss基础', link: '/web/unocssStudy' }
+          ]
+        }
+      ]
+    },
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    lastUpdatedText: '上次更新时间',
+    outlineTitle: '大纲'
   }
-}
+})
